4f0375777d8f1825ada0e40ce97ff0422394ced6
Talla Ndiaye <138068334+talla-ndiaye@users.noreply.github.com>
2025-05-22 18:17:22 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <ThemeProvider>
-      <div className=" container min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+      <div className="  min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
         <Navbar />
         <Hero />
         <About />
