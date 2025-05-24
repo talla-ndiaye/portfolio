35c71e8ffabd27c7a6acc19eda80a775a412495a
Talla ndiaye <ndiayetalla928@gmail.com>
2025-05-24 17:22:29 +0000

    modification du nom du site

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import { ThemeProvider } from './context/ThemeContext';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import { useEffect } from 'react';
 import About from './components/About';
-import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   useEffect(() => {
     // Update the document title
-    document.title = "Jean Dupont | Portfolio";
+    document.title = "Talla Ndiaye | Portfolio";
   }, []);
 
   return (
