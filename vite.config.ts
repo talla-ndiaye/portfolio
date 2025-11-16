14937199428d1d40e5dab91770ea30b0a2365d8d
Talla ndiaye <ndiayetalla928@gmail.com>
2025-11-16 23:12:16 +0000

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
+import generateSitemap from "vite-plugin-pages-sitemap";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+// Si vite-plugin-pages-sitemap n'a pas de types, ajoute dans src/globals.d.ts
+// declare module 'vite-plugin-pages-sitemap';
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
   },
-});
+  plugins: [
+    react(),
+    // plugins dev si nécessaire
+    {
+      name: "generate-sitemap",
+      closeBundle() {
+        generateSitemap({
+          hostname: "https://talla-ndiaye.dev",
+          readable: true,
+        });
+      },
+    },
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
